b951e7f9a1d6134c1947dd1417a3381eb99a72bd
Louis Moore <lmoore@dius.com.au>
2017-04-18 15:33:44 +1000

    Basic http serve

--- main.go
+++ main.go
@@ -7,5 +7,27 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan Message)
+var upgrader = websocket.Upgrader{}
+
+type Message struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+}
+
 func main() {
+	fs := http.FileServer(http.Dir("../public"))
+
+	http.Handle("/", fs)
+	http.Handle("/ws", handleConnections)
+
+	go handleMessages()
+
+	log.Println("http server started on :8000")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
