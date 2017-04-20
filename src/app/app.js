814c7d40d4db4ea855f6dbaf274619ac37905318
Louis Moore <lmoore@dius.com.au>
2017-04-20 16:23:00 +1000

    Remove materialize module because its a pos

--- src/app/app.js
+++ src/app/app.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import $ from 'jquery' 
 import emojione from 'emojione' 
 import CryptoJS from 'crypto-js'
-import Materialize from 'materialize-css'
 
 new Vue({
   el: "#app",
@@ -16,17 +15,17 @@ new Vue({
     joined: false
   },
 
-  created: () => {
-    var self = this;
+  created() {
+    let _self = this;
     this.ws = new WebSocket('ws://' + window.location.host + '/ws');
-    this.ws.addEventListener('message', (e) => {
-      var msg = JSON.parse(e.data)
-      console.log(msg)
-      this.chatContent += '<div class="chip">'
-          + '<img src="' + this.gravatarURL(msg.email) + '">'
-          + msg.username
-        + '</div>'
-        + emojione.toImage(msg.message) + '<br/>'
+    this.ws.addEventListener('message', function(e) {
+      let msg = JSON.parse(e.data);
+      _self.chatContent += `
+                            <div class="chip">
+                              <img src="${_self.gravatarURL(msg.email)}">
+                              ${msg.username}
+                            </div> ${emojione.toImage(msg.message)} <br/>
+                           `;
 
       let element = document.getElementById('chat-messages');
       element.scrollTop = element.scrollHeight;
@@ -34,37 +33,35 @@ new Vue({
   },
 
   methods: {
-    send: () => {
+    send() {
       if (this.newMsg != '') {
         this.ws.send(
           JSON.stringify({
-            email:    this.email,
+            email: this.email,
             username: this.username,
             message: $('<p>').html(this.newMsg).text()
-          })
-        );
+          }
+          ));
         this.newMsg = '';
       }
     },
 
-    join: () => {
-     // if (!this.email) {
-     //   console.log('no email');
-     //   //Materialize.toast('You must enter an email', 2000)
-     //   return
-     // }
-     // if (!this.username) {
-     //   console.log('no username');
-     //   //Materialize.toast('You must choose a username', 2000)
-     //   return
-     // }
+    join() {
+      if (!this.email) {
+        Materialize.toast('You must enter an email', 2000);
+        return
+      }
+      if (!this.username) {
+        Materialize.toast('You must choose a username', 2000);
+        return
+      }
       this.email = $('<p>').html(this.email).text();
       this.username = $('<p>').html(this.username).text();
       this.joined = true;
     },
-    gravatarURL: (email) => {
+
+    gravatarURL(email) {
       return 'http://www.gravatar.com/avatar/' + CryptoJS.MD5(email);
     }
   }
-
-})
+});
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -3,15 +3,15 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    entry: './app/app.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        alias: {
-              vue: 'vue/dist/vue.js'
-            }
+ entry: './app/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
     }
+  }
 };
 
